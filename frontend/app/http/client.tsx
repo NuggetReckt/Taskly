431fdaf3e25af3f62ca14794893a1481ba39f994
NuggetReckt <corto.morrow@gmail.com>
2026-01-08 19:47:35 +0100

    feat(App): Added board creation, modal UX, and enhanced API support for board details
    - Integrated modal for creating new boards.
    - Enabled fetching and displaying detailed board data, including members, labels, and lists.
    - Added `getBoardMembers` utility in API for streamlined member retrieval.
    - Updated global styles with modal support.
    - Adjusted API endpoints and backend logic for board creation and enhanced board detail retrieval.

--- frontend/app/http/client.tsx
+++ frontend/app/http/client.tsx
@@ -3,7 +3,7 @@ import {API_BASEURL, API_SECRET} from "@/app/secrets";
 
 export const client: AxiosInstance = axios.create({
     baseURL: API_BASEURL,
-    timeout: 1000,
+    timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
         'X-API-KEY': API_SECRET
